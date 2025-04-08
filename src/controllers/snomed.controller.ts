7071e336084d5e601092c56b1b9abf0c14f6df46
silviroa <silviroa@gmail.com>
2025-04-08 11:51:41 -0300

    fix: Cambios  en la busqueda de medicamentos (#26)
    * fix: Cambios  en la busqueda de medicamentos y al guardar nueva prescripcion
    
    * fix: agrega opcionales a interface de prescripciones

--- src/controllers/snomed.controller.ts
+++ src/controllers/snomed.controller.ts
@@ -5,15 +5,16 @@ import { ISnomedConcept } from '../interfaces/supply.interface';
 
 
 class snomedSupplyController implements BaseController {
-    public index = async (req: Request, res: Response): Promise<Response> => {
-        try {
-            const resp = await needle('get', `${process.env.ANDES_ENDPOINT}/core/term/snomed/expression?expression=<763158003`, { headers: { 'Authorization': process.env.JWT_MPI_TOKEN } });
-            const supplies: ISnomedConcept[] = resp.body;
-            return res.status(200).json(supplies);
-        } catch (e) {
-            return res.status(500).json({ mensaje: 'Error', error: e });
-        }
+  public index = async (req: Request, res: Response): Promise<Response> => {
+    try {
+      const search = req.query.search;
+      const resp = await needle('get', `${process.env.ANDES_ENDPOINT}/core/term/snomed?expression=<763158003:732943007=*,[0..0] 774159003=*, 763032000=*&search=${search}`);
+      const supplies: ISnomedConcept[] = resp.body;
+      return res.status(200).json(supplies);
+    } catch (e) {
+      return res.status(500).json({ mensaje: 'Error', error: e });
     }
+  }
 
     public create = async (req: Request, res: Response): Promise<Response> => {
         return res.status(200);
