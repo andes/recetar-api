932bb0b157061bb07a00e8edb9fd0f97cbf48028
mcele <celeste.ramos@fi.uncoma.edu.ar>
2025-08-07 14:54:28 -0300

    fix(paciente): actualiza pacientes con datos de andes

--- src/models/patient.model.ts
+++ src/models/patient.model.ts
@@ -6,102 +6,108 @@ import { obraSocialSchema } from './obraSocial.model';
 
 // Schema
 export const patientSchema = new Schema({
-  dni: {
-    type: String,
-  },
-  lastName: {
-    type: String,
-    required: '{PATH} is required'
-  },
-  firstName: {
-    type: String,
-    required: '{PATH} is required'
-  },
-  sex: {
-    type: String,
-    enum: ['Femenino', 'Masculino', 'Otro'],
-    required: '{PATH} is required'
-  },
-  status: {
-    type: String,
-    enum: ['Validado', 'Temporal'],
-  },
-  genero: {
-    type: String
-  },
-  nombreAutopercibido: {
-    type: String,
-    default: ''
-  },
-  idMPI: {
-    type: String,
-    default: ''
-  },
-  tipoDocumentoExtranjero: {
-    type: String,
-    default: ''
-  },
-  nroDocumentoExtranjero: {
-    type: String,
-    default: '',
-  },
-  obraSocial: {
-    type: obraSocialSchema,
-    default: null
-  },
-  estado: {
-    type: String,
-    enum: ['validado', 'temporal', 'recienNacido', 'extranjero', null],
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now
-  },
-  updatedAt: Date,
+    dni: {
+        type: String,
+    },
+    lastName: {
+        type: String,
+        required: '{PATH} is required'
+    },
+    firstName: {
+        type: String,
+        required: '{PATH} is required'
+    },
+    sex: {
+        type: String,
+        enum: ['Femenino', 'Masculino', 'Otro'],
+        required: '{PATH} is required'
+    },
+    status: {
+        type: String,
+        enum: ['Validado', 'Temporal'],
+    },
+    genero: {
+        type: String
+    },
+    nombreAutopercibido: {
+        type: String,
+        default: ''
+    },
+    idMPI: {
+        type: String,
+        default: ''
+    },
+    tipoDocumentoExtranjero: {
+        type: String,
+        default: ''
+    },
+    nroDocumentoExtranjero: {
+        type: String,
+        default: '',
+    },
+    obraSocial: {
+        type: obraSocialSchema,
+        default: null
+    },
+    estado: {
+        type: String,
+        enum: ['validado', 'temporal', 'recienNacido', 'extranjero', null],
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    updatedAt: Date,
 });
 
 // Model
 const Patient: Model<IPatient> = model<IPatient>('Patient', patientSchema);
 
-Patient.schema.method('findOrCreate', async function (patientParam: IPatient): Promise<IPatient> {
-  try {
-    // Buscar paciente en db local
-    let patient: IPatient | null = await Patient.findOne({ dni: patientParam.dni, sex: patientParam.sex });
+Patient.schema.method('findOrCreate', async (patientParam: IPatient): Promise<IPatient> => {
+    try {
+        // Buscar paciente en db local
+        let patient: IPatient | null = await Patient.findOne({ dni: patientParam.dni, sex: patientParam.sex });
+        const sexo = patientParam.sex.toLowerCase();
+        // Si no está local, buscar en MPI de Andes y guardar
+        const idPatient = patient ? patient.id : null;
+        if (!idPatient || patient?.idMPI === '') {
+            const url = `${process.env.ANDES_MPI_ENDPOINT}?search=${patientParam.dni}&sexo=${sexo}&activo=true&estado=validado`;
+            const resp = await needle('get', url, { headers: { Authorization: process.env.JWT_MPI_TOKEN } });
+            resp.body.forEach(async (item: any) => {
+                if (item.sexo === patientParam.sex.toLocaleLowerCase()) {
+                    patient = <IPatient>{
+                        dni: item.documento,
+                        firstName: item.nombre,
+                        lastName: item.apellido,
+                        sex: item.sexo[0].toUpperCase() + item.sexo.substr(1).toLowerCase(),
+                        status: item.estado[0].toUpperCase() + item.estado.substr(1).toLowerCase(),
+                        genero: item.genero,
+                        nombreAutopercibido: item.alias,
+                        idMPI: item.id,
+                        tipoDocumentoExtranjero: item.tipoIdentificacion || '',
+                        nroDocumentoExtranjero: item.numeroIdentificacion || '',
+                        estado: item.estado
+                    };
+                    if (!idPatient) {
+                        patient = await new Patient(patient);
+                        await patient.save();
+                    } else {
+                        await Patient.updateOne({ _id: idPatient }, patient);
 
-    // Si no está local, buscar en MPI de Andes y guardar
-    if (!patient) {
-      const resp = await needle("get", `${process.env.ANDES_MPI_ENDPOINT}?search=${patientParam.dni}&activo=true`, { headers: { 'Authorization': process.env.JWT_MPI_TOKEN } })
-      resp.body.forEach(async function (item: any) {
-        if (item.sexo === patientParam.sex.toLocaleLowerCase()) {
-          patient = <IPatient>{
-            dni: item.documento,
-            firstName: item.nombre,
-            lastName: item.apellido,
-            sex: item.sexo[0].toUpperCase() + item.sexo.substr(1).toLowerCase(),
-            status: item.estado[0].toUpperCase() + item.estado.substr(1).toLowerCase(),
-            genero: item.genero,
-            nombreAutopercibido: item.alias,
-            idMPI: item.id,
-            tipoDocumentoExtranjero: item.tipoIdentificacion || '',
-            nroDocumentoExtranjero: item.numeroIdentificacion || '',
-            estado: item.estado
-          }
-
-          patient = new Patient(patient);
-          await patient.save();
+                    }
+                }
+            });
         }
-      });
-    }
 
-    // Si tampoco no está local ni en MPI, crear uno nuevo
-    if (!patient) {
-      patient = new Patient(patientParam);
-      await patient.save();
+        // Si tampoco no está local ni en MPI, crear uno nuevo
+        if (!patient) {
+            patient = new Patient(patientParam);
+            await patient.save();
+        }
+        return patient;
+    } catch (err) {
+        throw new Error(`${err}`);
     }
-    return patient;
-  } catch (err) {
-    throw new Error(`${err}`);
-  }
 });
 
 export default Patient;
