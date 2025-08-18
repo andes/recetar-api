ba2e6e844dbe4dafb4d09f1f3af7a13ea6a98ae4
aldo <aldo.emanuel.matamala@gmail.com>
2025-08-18 16:09:35 -0300

    fix(REC-84): Fix practicas pdf y link publico

--- src/routes/public.ts
+++ src/routes/public.ts
@@ -1,11 +1,14 @@
 import { Router } from 'express';
 import certificateController from '../controllers/certificate.controller';
+import practiceController from '../controllers/practice.controller';
 
 class PublicRoutes{
 
   constructor(private router: Router = Router()){}
   public routes(): Router{
     this.router.get('/certificates/:id', certificateController.getById);
+        this.router.get('/practices/:id', practiceController.getById);
+
     return this.router;
   }
 }
