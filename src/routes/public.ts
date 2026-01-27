32a0722c76b7a1940fabd8aff2cbdc3409b7c4d7
ma7payne <matich04@gmail.com>
2026-01-27 13:18:05 -0300

    feat(REC): cambio de email con confirmación

--- src/routes/public.ts
+++ src/routes/public.ts
@@ -1,13 +1,15 @@
 import { Router } from 'express';
 import certificateController from '../controllers/certificate.controller';
 import practiceController from '../controllers/practice.controller';
+import usersController from '../controllers/users.controller';
 
 class PublicRoutes{
 
   constructor(private router: Router = Router()){}
   public routes(): Router{
+    this.router.post('/users/confirm-update', usersController.confirmEmailUpdate);
     this.router.get('/certificates/:id', certificateController.getById);
-        this.router.get('/practices/:id', practiceController.getById);
+    this.router.get('/practices/:id', practiceController.getById);
 
     return this.router;
   }
