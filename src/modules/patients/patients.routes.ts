7a70f83ba69ff1e5bb1586286fff4c3d47623971
ma7payne <matich04@gmail.com>
2026-05-29 09:54:46 -0300

    feat(REC): refactorizacion general del proyecto

--- src/modules/patients/patients.routes.ts
+++ src/modules/patients/patients.routes.ts
@@ -17,5 +17,6 @@ router.get('/:id', checkAuth, patientController.show);
 router.get('/dni/:dni', checkAuth, patientController.findByDni);
 router.post('/', checkAuth, validate(createPatientSchema), patientController.create);
 router.patch('/:id', checkAuth, validate(updatePatientSchema), patientController.update);
+router.patch('/:id', checkAuth, patientController.updatePartial);
 
 export default router;
