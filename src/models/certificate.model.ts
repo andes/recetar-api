ae5efd81af4c4f1836f7e7002e8567b2810a4c08
ma7payne <matich04@gmail.com>
2026-06-25 11:40:43 -0300

    feat(REC): agrega organizaciones al crear certificados

--- src/models/certificate.model.ts
+++ src/models/certificate.model.ts
@@ -26,6 +26,11 @@ const certificateSchema = new Schema({
         }]
     },
     certificate: { type: String },
+    organizacion: {
+        _id: { type: Schema.Types.ObjectId },
+        nombre: { type: String },
+        direccion: { type: String }
+    },
     startDate: { type: Date, required: true },
     cantDias: { type: Number, required: true },
     createdAt: {
